654730443dd69d858354beafc712eb393ea39f47
Suraj Kalu <suraz.kalu@gmail.com>
2022-08-15 08:16:58 +0000

    Finish django registration

--- blango/settings.py
+++ blango/settings.py
@@ -172,6 +172,8 @@ class Dev(Configuration):
       'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
   ]
 
+  EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+  ACCOUNT_ACTIVATION_DAYS = 7
 
 class Prod(Dev):
   DEBUG = False
